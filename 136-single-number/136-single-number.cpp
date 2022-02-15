aa5c99701ce17ef194ac1360473128a31a907cd0
Abhinav-Vijay-Walunj <64826301+Abhinav-Vijay-Walunj@users.noreply.github.com>
2022-02-15 08:19:02 +0530

    Time: 19 ms (78.37%), Space: 16.9 MB (50.83%) - LeetHub

---
+++ 136-single-number/136-single-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int res=0;
+        for(auto i=nums.begin();i!=nums.end();i++)
+        {
+            res^=(*i);
+        }
+        return res;
+        
+    }
+};
